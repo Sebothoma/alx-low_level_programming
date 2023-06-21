b0bdcbce38632fc72c535ef152246a09bdfa5197
Sebothoma <sebothomalister@gmail.com>
2023-06-21 01:41:56 -0700

    variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,30 +3,40 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print three nimber combination
+ *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int d, p, q;
-for (d  '0'; d < '9'; p++)
+int d = '0';
+int p = '0';
+int q = '0';
+
+while (d <= '7')
 {
-for (p = d + 1; p <= '9'; p++)
+while (p <= '8')
 {
-for (q = p +1; q <= '9'; q++)
+while (q <= '9')
 {
-if ((p != d) != q)
+if (d < p && p < q)
 {
 putchar(d);
 putchar(p);
 putchar(q);
-if (d == '7' && p == '8')
-continue;
+if (!(d == '7' && p == '8' && q == '9'))
+{
 putchar(',');
 putchar(' ');
 }
 }
+q++
 }
+q = '0';
+p++;
+}
+p = '0';
+d++;
 }
 putchar('\n');
 return (0);
