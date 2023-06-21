7ec48b1cc0d26d498773f39d8e7ec6b91ebc4e51
Sebothoma <sebothomalister@gmail.com>
2023-06-21 12:37:07 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute valueof an interger.
+ * @c: the number to be computed
+ * Return: absolute value of number or zero
+
+in _ab(int c)
+{
+if (c < 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
