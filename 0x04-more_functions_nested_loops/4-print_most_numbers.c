66acee61dd78b462e34d650b89a5da1b6c0ec036
Sebothoma <sebothomalister@gmail.com>
2023-06-23 13:01:07 -0700

    0x04

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,19 +6,20 @@
  * Return: the numbers since 0 up to 9
  */
 
-void print_most_numbers(void)
+void print_most_numbers(void);
+
 {
 int x = 0;
 for (; x <= 0; x++)
 {
 if (x == 2 || x == 4)
 {
-continue
+continue;
 }
 else
 {
-_putchar(x +'0');
+_putchar(x + '0');
 }
 }
-_puchar('\n');
+_putchar('\n');
 }
