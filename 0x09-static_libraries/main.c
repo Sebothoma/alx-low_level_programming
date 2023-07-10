45a21f21d402c03bd230f6ba486960df961f427b
Sebothoma <sebothomalister@gmail.com>
2023-07-10 02:11:04 -0700

    - Kevin Mitnick

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
+    _puts("\"At the end of the day, my goal was to be the best hacker - Kevin Mitnick\"\n\t");
     return (0);
 }
